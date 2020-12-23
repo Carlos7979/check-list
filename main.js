d32eef767397b473e720922c1bf49177c75b115c
Carlos7979 <carloscorredorwg@google.com>
2020-12-22 22:38:35 -0400

    put all code who construct each block inside a blockConstructor function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function  main() {
+  let blocksNumber;
   let allowInitialData = true;
-  const initialDescripcions = [
+  const initialDescriptions = [
     [ {
       name: 'Carlos',
       blocksNum: 4
@@ -17,7 +18,7 @@ function  main() {
       'Inglés' ],
     [ 'Actividades',
       'Resúmenes',
-      'Codecademy',
+      'Programación',
       'Alterna',
       'Abuelo',
       'Actividades de repaso (AR)',
@@ -90,82 +91,87 @@ function  main() {
       }
     });
   };
-  const blocksContainer = document.getElementById('blocks-container');
-  for(let i = 0; i < 4; i++) {
-    const block = document.createElement("DIV");
-        block.setAttribute("id", `block-${i+1}`);
-        block.setAttribute("class", 'block');
-
-            const title = document.createElement("DIV");  
-                title.setAttribute("id", `title-${i+1}`);
-                title.setAttribute("class", 'title');
-                    const heading = document.createElement("h3");  
-                    heading.setAttribute("id", `h3-${i+1}`);
-                    heading.setAttribute("class", 'title');
-                title.appendChild(heading);
-                allowInitialData ? heading.innerHTML = initialDescripcions[i + 1][0] : heading.innerHTML = `Título ${i+1}`
-                    const input = document.createElement("input");
-                    input.setAttribute("id", `input-${i + 1}-t`);
-                    input.setAttribute("class", 'title-i');
-                    input.setAttribute("maxlength", '60');
-                    input.setAttribute("type", 'hidden');
-                title.appendChild(input);
-                    const buttonInsert = document.createElement("button");
-                    buttonInsert.setAttribute("id", `insert-${i + 1}-t`);
-                    buttonInsert.setAttribute("class", 'button-edit');
-                    buttonInsert.setAttribute("hidden", 'hidden');
-                    buttonInsert.innerHTML = 'insertar';
-                title.appendChild(buttonInsert);
-                    const buttonDelete = document.createElement("button");
-                    buttonDelete.setAttribute("id", `delete-${i + 1}-t`);
-                    buttonDelete.setAttribute("class", 'button-delete');
-                    buttonDelete.setAttribute("hidden", 'hidden');
-                    buttonDelete.innerHTML = 'borrar';
-                title.appendChild(buttonDelete);
-        block.appendChild(title);
-        descriptionInputControls(title);
-            const list = document.createElement("DIV");  
-                list.setAttribute("id", `list-${i+1}`);
-                list.setAttribute("class", 'list');
-                    const ol = document.createElement("ol");  
-                    ol.setAttribute("id", `ol-${i+1}`);
-                    for(let j = 0; j < 9; j++) {
-                      const li = document.createElement("li");
-                      li.setAttribute("id", `li-${j+1 +i*9}`);
-                          const check = document.createElement("DIV");
-                          check.setAttribute("id", `check-${j+1 +i*9}`);
-                          check.setAttribute("class", 'check');
-                      li.appendChild(check);
-                          const description = document.createElement("DIV");
-                          description.setAttribute("id", `description-${j+1 +i*9}`);
-                          description.setAttribute("class", 'description');
-                      li.appendChild(description);
-                      if(allowInitialData && (initialDescripcions[i + 1].length > j + 1)) {
-                        description.innerHTML = initialDescripcions[i + 1][j + 1];
+  
+  allowInitialData && (initialDescriptions.length -1) ? blocksNumber = initialDescriptions.length : blocksNumber = 4;
+  function blockConstructor(n) {
+    const blocksContainer = document.getElementById('blocks-container');
+    for(let i = 0; i < n; i++) {
+      const block = document.createElement("DIV");
+          block.setAttribute("id", `block-${i+1}`);
+          block.setAttribute("class", 'block');
+  
+              const title = document.createElement("DIV");  
+                  title.setAttribute("id", `title-${i+1}`);
+                  title.setAttribute("class", 'title');
+                      const heading = document.createElement("h3");  
+                      heading.setAttribute("id", `h3-${i+1}`);
+                      heading.setAttribute("class", 'title');
+                  title.appendChild(heading);
+                  allowInitialData ? heading.innerHTML = initialDescriptions[i + 1][0] : heading.innerHTML = `Título ${i+1}`
+                      const input = document.createElement("input");
+                      input.setAttribute("id", `input-${i + 1}-t`);
+                      input.setAttribute("class", 'title-i');
+                      input.setAttribute("maxlength", '60');
+                      input.setAttribute("type", 'hidden');
+                  title.appendChild(input);
+                      const buttonInsert = document.createElement("button");
+                      buttonInsert.setAttribute("id", `insert-${i + 1}-t`);
+                      buttonInsert.setAttribute("class", 'button-edit');
+                      buttonInsert.setAttribute("hidden", 'hidden');
+                      buttonInsert.innerHTML = 'insertar';
+                  title.appendChild(buttonInsert);
+                      const buttonDelete = document.createElement("button");
+                      buttonDelete.setAttribute("id", `delete-${i + 1}-t`);
+                      buttonDelete.setAttribute("class", 'button-delete');
+                      buttonDelete.setAttribute("hidden", 'hidden');
+                      buttonDelete.innerHTML = 'borrar';
+                  title.appendChild(buttonDelete);
+          block.appendChild(title);
+          descriptionInputControls(title);
+              const list = document.createElement("DIV");  
+                  list.setAttribute("id", `list-${i+1}`);
+                  list.setAttribute("class", 'list');
+                      const ol = document.createElement("ol");  
+                      ol.setAttribute("id", `ol-${i+1}`);
+                      for(let j = 0; j < 9; j++) {
+                        const li = document.createElement("li");
+                        li.setAttribute("id", `li-${j+1 +i*9}`);
+                            const check = document.createElement("DIV");
+                            check.setAttribute("id", `check-${j+1 +i*9}`);
+                            check.setAttribute("class", 'check');
+                        li.appendChild(check);
+                            const description = document.createElement("DIV");
+                            description.setAttribute("id", `description-${j+1 +i*9}`);
+                            description.setAttribute("class", 'description');
+                        li.appendChild(description);
+                        if(allowInitialData && (initialDescriptions[i + 1].length > j + 1)) {
+                          description.innerHTML = initialDescriptions[i + 1][j + 1];
+                        }
+                            const input = document.createElement("input");
+                            input.setAttribute("id", `input-${j+1 +i*9}`);
+                            input.setAttribute("maxlength", '60');
+                            input.setAttribute("type", 'hidden');
+                        li.appendChild(input);
+                            const buttonInsert = document.createElement("button");
+                            buttonInsert.setAttribute("id", `insert-${j+1 +i*9}`);
+                            buttonInsert.setAttribute("class", 'button-edit');
+                            buttonInsert.setAttribute("hidden", 'hidden');
+                            buttonInsert.innerHTML = 'insertar';
+                        li.appendChild(buttonInsert);
+                            const buttonDelete = document.createElement("button");
+                            buttonDelete.setAttribute("id", `delete-${j+1 +i*9}`);
+                            buttonDelete.setAttribute("class", 'button-delete');
+                            buttonDelete.setAttribute("hidden", 'hidden');
+                            buttonDelete.innerHTML = 'borrar';
+                        li.appendChild(buttonDelete);
+                        descriptionInputControls(li);
+                        ol.appendChild(li);
                       }
-                          const input = document.createElement("input");
-                          input.setAttribute("id", `input-${j+1 +i*9}`);
-                          input.setAttribute("maxlength", '60');
-                          input.setAttribute("type", 'hidden');
-                      li.appendChild(input);
-                          const buttonInsert = document.createElement("button");
-                          buttonInsert.setAttribute("id", `insert-${j+1 +i*9}`);
-                          buttonInsert.setAttribute("class", 'button-edit');
-                          buttonInsert.setAttribute("hidden", 'hidden');
-                          buttonInsert.innerHTML = 'insertar';
-                      li.appendChild(buttonInsert);
-                          const buttonDelete = document.createElement("button");
-                          buttonDelete.setAttribute("id", `delete-${j+1 +i*9}`);
-                          buttonDelete.setAttribute("class", 'button-delete');
-                          buttonDelete.setAttribute("hidden", 'hidden');
-                          buttonDelete.innerHTML = 'borrar';
-                      li.appendChild(buttonDelete);
-                      descriptionInputControls(li);
-                      ol.appendChild(li);
-                    }
-                list.appendChild(ol);
-                check(list);
-        block.appendChild(list);
-        blocksContainer.appendChild(block);
-  }
+                  list.appendChild(ol);
+                  check(list);
+          block.appendChild(list);
+          blocksContainer.appendChild(block);
+    }
+  };
+  blockConstructor(blocksNumber);
 }
